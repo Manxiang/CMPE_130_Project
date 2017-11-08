cb38f3cc8bf9efe9ed801a41a1c2a7d215bf6cf3
Manxiang Zhang <manxiangzhang@gmail.com>
2017-11-07 16:51:23 -0800

    update layout/setted up connection to mongoDB

---
+++ Flight_Search/server/models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  movie: require('./Movie.js')
+};
+// a registry for all of the models in the system
---
+++ Flight_Search/server/routes.js
@@ -0,0 +1,5 @@
+//assign controller to certain route of the app
+
+module.exports = {
+  'movie': require('./controllers/MovieController')
+}; //at the route /movie, I want to access this controller
