423ed654ecfa9442c6dec09752cc8d8f8a07918a
Manxiang Zhang <manxiangzhang@gmail.com>
2017-12-02 12:18:36 -0800

    AdditionalSearchOption/UI Update

--- Flight_Search/index.js
+++ Flight_Search/index.js
@@ -2,21 +2,17 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 
+/**********************************************************************
+***********************************************************************
+*********************Algorithm********************************/
 const sGraph = require('node-dijkstra');
 const sRoute = new sGraph();   //shortest path
 
-// const aGraph = require('node-all-paths');
-// const aRoute = new aGraph();
-
 sRoute.addNode('LosAngeles', {SanFrancisco:1});
 sRoute.addNode('SanFrancisco', { LosAngeles:1, SanDiego:2, Sacramento: 4 })
-sRoute.addNode('SanDiego', { SanDiego:2, Sacramento:1 })
+sRoute.addNode('SanDiego', { Sacramento:1 })
 sRoute.addNode('Sacramento', { SanDiego:1, SanFrancisco:4 })
-
-// aRoute.addNode('LosAngeles', {SanFrancisco:1});
-// aRoute.addNode('SanFrancisco', { LosAngeles:1, SanDiego:2, Sacramento: 4 })
-// aRoute.addNode('SanDiego', { SanDiego:2, Sacramento:1 })
-// aRoute.addNode('Sacramento', { SanDiego:1, SanFrancisco:4 })
+//*******************END**************************************
 
 var app = express();
 
@@ -27,23 +23,40 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
-
+app.use(express.static('views'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//****************************************GET/POST REQUEST******************
+
 app.get('/', function (req, res) {
     res.render('home');
-});
+}); //get information from home page
 
 app.post('/resultPage', function(req, res){
+var all = ["LosAngeles","SanFrancisco", "SanDiego", "Sacramento"]; //add cities to this string array
 var start = req.body.start_location;
 var end = req.body.end_location;
-var shortest = sRoute.path(start, end);
-// var allpaths = aRoute.path(start, end);
-res.render('resultPage', {data1: shortest});
-});
+var distance = req.body.distance;
 
+var destination = [];
+
+var shortest = sRoute.path(start, end, {cost: true, trim: true});
+
+var i;
+for(i = 0; i < all.length; i++ ){
+  var short_temp = sRoute.path(start, all[i], {cost: true});
+  if(short_temp.cost <= distance & short_temp.cost != 0){
+      destination.push(all[i]);
+  }
+}  //get all possible destination
+
+res.render('resultPage', {data1: shortest, data2: distance, data3: destination, start: start, end: end});
+});   //post result to resultpage
+
+
+//*******************************************GET PORT**********************
 var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
